157a4093a3323625185b8589ea813baa8a5485f3
topoftheyear <topoftheyear@Gmail.com>
2019-02-22 18:19:04 -0600

    Adds start to C# emailing thing

---
+++ ReportGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Mail;
+
+public class ReportGenerator
+{
+    private string[] addresses;
+    private string subject;
+    private string content;
+
+    private string[] email_list = { "riley.conlin@ndsu.edu", "jacob.baumann@ndsu.edu" };
+
+	static void Main()
+    {
+        foreach (string email_to in email_list)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(new MailAddress(email_to));
+            mail.Subject = "This is a code test :)";
+            mail.From = new MailAddress("riley.conlin@ndsu.edu");
+            mail.Body = "Test";
+            SmtpClient smtp = new SmtpClient("SMTP Server");
+            smtp.Send(mail);
+        }
+    }
+}

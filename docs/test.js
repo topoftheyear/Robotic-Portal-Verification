77e1d3a3ff347358cdda9dcad674d1380570bfcc
topoftheyear <topoftheyear@Gmail.com>
2019-03-01 19:47:25 -0600

    Corrects pointer to Javascript file

--- docs/test.js
+++ docs/test.js
@@ -1,5 +1,26 @@
 window.onload = function (){
-    var data = {TIN: 123456789, NPI: 1234567891, PTAN: "1234abcd", BenHICN: "123456789a", BenMBI: "123456abcdef", BenName: "Bob Robertson", BenDOB: "01/01/2000", BenSSN: 123456789, ICN: 123456789123456, FCN: 12345678912345, BenDOS: "01/01/0001"};
+    var data = [{TIN:       123456789, 
+                NPI:        1234567891, 
+                PTAN:       "1234abcd", 
+                BenHICN:    "123456789a", 
+                BenMBI:     "123456abcdef", 
+                BenName:    "Bob Robertson", 
+                BenDOB:     "01/01/2000", 
+                BenSSN:     123456789, 
+                ICN:        123456789123456, 
+                FCN:        12345678912345, 
+                BenDOS:     "01/01/0001"},
+                {TIN:       123456789, 
+                NPI:        1234567891, 
+                PTAN:       "1234abcd", 
+                BenHICN:    "123456789a", 
+                BenMBI:     "123456abcdef", 
+                BenName:    "Bob Robertson", 
+                BenDOB:     "01/01/2000", 
+                BenSSN:     123456789, 
+                ICN:        123456789123456, 
+                FCN:        12345678912345, 
+                BenDOS:     "01/01/0001"}];
     
     var subbutton = document.getElementById("SubmitButton");
     subbutton.addEventListener('click', function(){
